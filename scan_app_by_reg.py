ee8eb1d8860247df4c3a7f1c1c1a8ddd9e1fc792
Deng-Xian-Sheng <x17615848429@sina.com>
2024-05-23 17:17:58 +0800

    emmmm

--- scan_app_by_reg.py
+++ scan_app_by_reg.py
@@ -12,3 +12,6 @@ import winreg
 # HKEY_CURRENT_USER\Software\Microsoft\Installer\Products 
 # HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Persisted
 winreg.LoadKey()
+# 不在区分这个东西是谁的，而是判断这个东西能不能迁移，把不能迁移的挑出来。
+# usmt必须要储存文件才能迁移吗？有硬连接迁移的办法，但是适用性低。
+# 让gpt列举注册表项，并说明子级结构
